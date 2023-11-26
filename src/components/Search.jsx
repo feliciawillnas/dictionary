23e39a51160e240a9fc65bad8411650b02f88175
felicia <feliciawillnas@gmail.com>
2023-11-26 17:55:57 +0100

    styling finished

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,28 @@
 import "../css/Search.css";
 
 export default function Search() {
-  return <input placeholder="Search" type="text" />;
+  return (
+    <>
+      <div className="input-container">
+        <input placeholder="Search" type="text" />
+        <button className="search-button">
+          <svg
+            class="w-6 h-6 text-gray-800 dark:text-white svg-search"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="3"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </button>
+      </div>
+    </>
+  );
 }
--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,29 +1,42 @@
-import '../css/SearchResults.css';
+import "../css/SearchResults.css";
 
 export default function SearchResults() {
   return (
     <>
       <div className="search-results">
-        <div className='flex word-title-svg-wrapper'>
-          <h1>Word</h1>
-          <svg
-            class="w-6 h-6 text-gray-800 dark:text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 21 20"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="3"
-              d="m11.479 1.712 2.367 4.8a.532.532 0 0 0 .4.292l5.294.769a.534.534 0 0 1 .3.91l-3.83 3.735a.534.534 0 0 0-.154.473l.9 5.272a.535.535 0 0 1-.775.563l-4.734-2.49a.536.536 0 0 0-.5 0l-4.73 2.487a.534.534 0 0 1-.775-.563l.9-5.272a.534.534 0 0 0-.154-.473L2.158 8.48a.534.534 0 0 1 .3-.911l5.294-.77a.532.532 0 0 0 .4-.292l2.367-4.8a.534.534 0 0 1 .96.004Z"
-            />
-          </svg>
+        <div style={{ padding: "3rem 3rem 2rem 3rem" }}>
+          <div className="flex word-title-svg-wrapper">
+            <h1 style={{ fontWeight: "bold" }}>Word</h1>
+            <div className="svg-wrapper">
+              <svg
+                class="w-6 h-6 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 21 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="3"
+                  d="m11.479 1.712 2.367 4.8a.532.532 0 0 0 .4.292l5.294.769a.534.534 0 0 1 .3.91l-3.83 3.735a.534.534 0 0 0-.154.473l.9 5.272a.535.535 0 0 1-.775.563l-4.734-2.49a.536.536 0 0 0-.5 0l-4.73 2.487a.534.534 0 0 1-.775-.563l.9-5.272a.534.534 0 0 0-.154-.473L2.158 8.48a.534.534 0 0 1 .3-.911l5.294-.77a.532.532 0 0 0 .4-.292l2.367-4.8a.534.534 0 0 1 .96.004Z"
+                />
+              </svg>
+            </div>
+          </div>
+          <h4>Type</h4>
+          <h3>Pronounciation word word</h3>
         </div>
-        <h3>Type</h3>
-        <h3>Pronounciation word word</h3>
+        <div
+          style={{ borderBottom: "1px solid #C8C8C8", display: "block" }}
+        ></div>
+        <div style={{ padding: "2rem 3rem 3rem 3rem" }}>
+          <h2>Description</h2>
+          <h2>
+            <li>Description</li>
+          </h2>
+        </div>
       </div>
     </>
   );
